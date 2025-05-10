eb45caa08f0e349526ae2e06b47539fa786ce46a
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-05-10 14:29:49 -0400

    Update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -15,6 +15,8 @@ typedef SwagSong =
 	var needsVoices:Bool;
 	var speed:Float;
 
+	var mania:Int;
+	
 	var player1:String;
 	var player2:String;
 	var stage:String;
@@ -37,6 +39,45 @@ class Song
 	public var stage:String = '';
 	public var gf:String = 'gf';
 	public var vocalVolume:Float = 1.0;
+
+	private static function onLoadJson(songJson:Dynamic) // Convert old charts to newest format
+	{
+		if(songJson.gfVersion == null)
+		{
+			songJson.gfVersion = songJson.player3;
+			songJson.player3 = null;
+		}
+
+		if(songJson.events == null)
+		{
+			songJson.events = [];
+			for (secNum in 0...songJson.notes.length)
+			{
+				var sec:SwagSection = songJson.notes[secNum];
+
+				var i:Int = 0;
+				var notes:Array<Dynamic> = sec.sectionNotes;
+				var len:Int = notes.length;
+				while(i < len)
+				{
+					var note:Array<Dynamic> = notes[i];
+					if(note[1] < 0)
+					{
+						songJson.events.push([note[0], [[note[2], note[3], note[4]]]]);
+						notes.remove(note);
+						len = notes.length;
+					}
+					else i++;
+				}
+			}
+		}
+
+		// fix mania being null (for non ek charts)
+		if (songJson.mania == null)
+		{
+			songJson.mania = 3;
+		}
+	}
 
 	public function new(song, notes, bpm)
 	{
